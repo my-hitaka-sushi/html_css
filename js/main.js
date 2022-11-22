6d0491df010006f317afbf42b6be881bebfeaf18
marouane <uanemaro216@gmail.com>
2022-11-22 15:26:49 +0100

    update responsive

--- js/main.js
+++ js/main.js
@@ -114,7 +114,7 @@ function addItemToCart(title, price, imageSrc) {
       <span class="cart-price cart-column">${price}</span>
       <div class="cart-quantity cart-column">
           <input class="cart-quantity-input" type="number" value="1">
-          <button class="btn btn-danger" type="button">REMOVE</button>
+          <button class="btn btn-danger" type="button">X</button>
       </div>`
   cartRow.innerHTML = cartRowContents
   cartItems.append(cartRow)
